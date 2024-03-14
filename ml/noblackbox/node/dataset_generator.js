a53af52547001682bd12fbb5aa4a97a4e5cd4096
yaonur <ya_onur@hotmailc.om>
2024-03-15 02:42:39 +0300

    ss

--- ml/noblackbox/node/dataset_generator.js
+++ ml/noblackbox/node/dataset_generator.js
@@ -1,3 +1,4 @@
+const draw=require('../common/draw.js')
 const constants={}
 constants.DATA_DIR="../data"
 constants.RAW_DIR=constants.DATA_DIR+"/raw"
@@ -16,12 +17,31 @@ fileNames.forEach(fn=>{
 		constants.RAW_DIR+"/"+fn
 	)
 	const {session,student,drawings}=JSON.parse(content)
-	samples.push({
-		id,
-		session,
-		student,
-		drawings
-	})
-	id++
+	for(let label in drawings){
+		samples.push({
+			id,
+			label,
+			student_name:student,
+			student_id:session
+		})
+		const paths = drawings[label]
+		fs.writeFileSync(
+			constants.JSON_DIR+"/"+id+".json",
+			JSON.stringify(drawings[label])
+		)
+		generateImageFile(
+			constants.IMG_DIR+"/"+id+".png",
+			paths
+		)
+		id++
+	}
 })
+fs.writeFileSync(constants.SAMPLES,JSON.stringify(samples))
+
+function generateImageFile(outFile,paths){
+	draw.paths(ctx,paths)
+
+	const buffer= canvas.toBuffer("image/png")
+	fs.writeFileSync(outFile,buffer)
+}
 
