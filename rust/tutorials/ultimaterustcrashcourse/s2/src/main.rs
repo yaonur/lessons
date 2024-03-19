b7431fd5675c6dda9da81203cde9d3b8e1b83bde
yaonur <ya_onur@hotmailc.om>
2024-03-20 02:57:47 +0300

    ss

--- rust/tutorials/ultimaterustcrashcourse/s2/src/main.rs
+++ rust/tutorials/ultimaterustcrashcourse/s2/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    shadow();
+    memory_safety_init_var();
+}
+fn shadow() {
+    let x = 10;
+    {
+        let x = 20;
+        println!("{}", x); // 20
+    }
+    println!("{}", x); // 10
+}
+
+fn memory_safety_init_var(){
+    let mut enigma: i32;
+    // println!("{}", enigma); // wont work
+    if true {
+        enigma = 3;
+        println!("{}", enigma);
+    } else {
+        enigma = 7;
+    }
+    println!("{}", enigma); // error if not else block before build rust saves day 
 }
